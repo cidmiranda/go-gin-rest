fbb33d41b0af8beb81ddfc6a78dd8759a4954712
Cid Miranda <cidmiranda@gmail.com>
2025-03-10 11:48:34 -0300

    tests: auth service test

--- internal/core/service/auth.go
+++ internal/core/service/auth.go
@@ -8,11 +8,17 @@ import (
 	"github.com/cidmiranda/go-fiber-ws/internal/core/util"
 )
 
+/**
+ * AuthService implements port.AuthService interface
+ * and provides an access to the user repository
+ * and token service
+ */
 type AuthService struct {
 	repo port.UserRepository
 	ts   port.TokenService
 }
 
+// NewAuthService creates a new auth service instance
 func NewAuthService(repo port.UserRepository, ts port.TokenService) *AuthService {
 	return &AuthService{
 		repo,
@@ -20,6 +26,7 @@ func NewAuthService(repo port.UserRepository, ts port.TokenService) *AuthService
 	}
 }
 
+// Login gives a registered user an access token if the credentials are valid
 func (as *AuthService) Login(ctx context.Context, email, password string) (string, error) {
 	user, err := as.repo.GetUserByEmail(ctx, email)
 	if err != nil {
